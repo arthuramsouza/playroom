1b5257eec1410644f41d082b5c2966f979998dfd
Arthur Menezes <arthuramsouza@gmail.com>
2020-03-17 04:42:16 -0300

    Add new route and template, add gitignore

--- app.py
+++ app.py
@@ -9,11 +9,17 @@ class Game:
         self.genre = genre
         self.platform = platform
 
+
 @app.route('/')
-def hello_world():
+def list():
     game1 = Game('Halo 2', 'Sci-fi', 'Xbox')
     game2 = Game('Red Dead Redemption', 'Action', 'Xbox 360')
     return render_template('list.html', title='Playroom', games=[game1, game2])
+
+
+@app.route('/new')
+def hello_world():
+    return render_template('new.html', title='New game')
 
 
 if __name__ == '__main__':
