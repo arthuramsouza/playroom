a34ad7cd4437922f66dbb764119ba5a161e5660d
Arthur Menezes <arthuramsouza@gmail.com>
2020-03-20 01:33:11 -0300

    Add Flask's secret key, create '/logout' route, add flash messages and fix the 'list' template's table responsiveness

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
-from flask import Flask, redirect, render_template, request
+import os
+from flask import Flask, flash, redirect, render_template, request, session
 
 app = Flask(__name__)
+app.secret_key = os.urandom(16)
 
 
 class Game:
@@ -26,9 +28,19 @@ def login():
 @app.route('/authenticate', methods=['POST'])
 def authenticate():
     if request.form['password'] == 'admin':
+        session['logged_user'] = request.form['username']
+        flash(request.form['username'] + ', you are now logged in!', 'success')
         return redirect('/')
     else:
+        flash('Something went wrong. Please, check your credentials and try again.', 'danger')
         return redirect('/login')
+
+
+@app.route('/logout')
+def logout():
+    session['logged_user'] = None
+    flash('You are now logged out', 'success')
+    return redirect('/')
 
 
 @app.route('/new')
